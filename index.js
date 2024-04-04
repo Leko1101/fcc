5496fb16e3e5762b6a43082607e8aabf292bdd68
Leko1101 <165515103+Leko1101@users.noreply.github.com>
2024-04-04 20:28:46 +0000

    changed the way how code works(not functioal)

--- index.js
+++ index.js
@@ -1,52 +1,72 @@
-function calculationCycle() {
-
-let gyroTilt = Math.random() * 360          //replace with real input
-let controlTilt = Math.random() * 360       //replace with real input
-
-let gyroRoll = Math.random() * 360          //replace with real input
-let controlRoll = Math.random() * 360       //replace with real input
-
-let gyroYaw = Math.random() * 360           //replace with real input
-let controlYaw = Math.random() * 360        //replace with real input
-
-
-console.log("gyroTilt: ", gyroTilt)             //for data
-console.log("controlTilt: ", controlTilt)       //for data
-
-console.log("gyroRoll: ",gyroRoll)              //for data
-console.log("controllRoll: ", controlRoll)      //for data
-
-console.log("gyroYaw: ", gyroYaw)               //for data
-console.log("controlYaw:", controlYaw)          //for data
-
-
-if(controlTilt < gyroTilt) {                    //replace with real outputs, 2d object to the right angle ccw
-    console.log("tilt down ", controlTilt - gyroTilt)
-} else if(controlTilt > gyroTilt) {             
-    console.log("tilt up ", controlTilt - gyroTilt)
-} else {
-    console.log("on targeted tilt")
+function cycle() {
+    inputCycle()
+    calculationCycle()
 }
+const intervalId = setInterval(cycle, 1000)
 
-if(controlRoll < gyroRoll) {                    //replace with real outputs, angles counted ccw
-    console.log("roll clockwise ", controlRoll - gyroRoll)
-} else if(controlRoll > gyroRoll) {
-    console.log("roll anti-clockwise ", controlRoll - gyroRoll)
-} else {
-    console.log("on targeted roll")
-}
+function inputCycle() {
 
-if(controlYaw < gyroYaw) {                      //replace with real outputs, angles counted ccw
-    console.log("yaw right ", controlYaw - gyroYaw)
-} else if(controlYaw > gyroYaw) {
-    console.log("yaw left ", controlYaw - gyroYaw)
-} else {
-    console.log("on targeted yaw")
-}
+    let gyroTilt = Math.random() * 360            //replace with real input
+    const controlTilt = Math.random() * 360       //replace with real input
+    let targetTilt = gyroTilt + controlTilt
+
+    let gyroRoll = Math.random() * 360            //replace with real input
+    const controlRoll = Math.random() * 360       //replace with real input
+    let targetRoll = gyroRoll + controlRoll
+
+    let gyroYaw = Math.random() * 360             //replace with real input
+    const controlYaw = Math.random() * 360        //replace with real input
+    let targetYaw  = gyroYaw  + controlYaw
 
 
-gyroYaw = 0                         //has to reset real gyro outputs for implementations
-gyroTilt = 0                        //has to reset real gyro outputs for implementations
-gyroRoll = 0                        //has to reset real gyro outputs for implementations
-}
-const intervalId = setInterval(calculationCycle, 1000)
+    console.log("gyroTilt: ", gyroTilt)             //for data
+    console.log("controlTilt: ", controlTilt)       //for data
+    console.log("targetTilt: ", targetTilt)         //for data
+
+    console.log("gyroRoll: ",gyroRoll)              //for data
+    console.log("controlRoll: ", controlRoll)      //for data
+    console.log("targetRoll: ", targetRoll)         //for data
+
+    console.log("gyroYaw: ", gyroYaw)               //for data
+    console.log("controlYaw:", controlYaw)          //for data
+    console.log("targetYaw: ", targetYaw)           //for data
+
+    }
+function calculationCycle(gyroRoll, gyroTilt, gyroYaw,
+     controlRoll, controlTilt, controlYaw, targetRoll, targetTilt, targetTilt) {
+        
+
+        targetTilt = 0
+        gyroTilt = gyroTilt - targetTilt
+    
+        targetRoll = 0
+        gyroRoll = gyroRoll - targetRoll
+    
+        targetYaw = 0
+        gyroYaw = gyroYaw - targetYaw
+    
+        if(targetTilt < gyroTilt) {
+            console.log("tilt down: ", gyroTilt - targetTilt)
+        } else if(targetTilt > gyroTilt) {
+            console.log("tilt up: ", gyroTilt - targetTilt)
+        } else {
+            console.log("on target tilt")
+        }
+    
+        if(targetRoll < gyroRoll) {
+            console.log("roll clockwise: ", gyroRoll - targetRoll)
+        } else if(targetRoll > gyroRoll) {
+            console.log("roll anti-clockwise: ", gyroRoll - targetRoll)
+        } else {
+            console.log("on target roll")
+        }
+    
+        if(targetYaw < gyroYaw) {
+            console.log("yaw left: ", gyroYaw - targetYaw)
+        } else if(targetYaw > gyroYaw) {
+            console.log("yaw right: ", gyroYaw - targetYaw)
+        } else {
+            console.log("on target yaw")
+        }
+    
+        }
