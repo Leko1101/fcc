e9e9acbd35b9208502eabf2d00e475ea75c76170
Leko1101 <165515103+Leko1101@users.noreply.github.com>
2024-04-04 20:26:41 +0000

    changed way how code works (not functional currently)

--- index.js
+++ index.js
@@ -1,37 +1,52 @@
-let status = "on"
+function calculationCycle() {
 
-while(status === "on") {
+let gyroTilt = Math.random() * 360          //replace with real input
+let controlTilt = Math.random() * 360       //replace with real input
 
-function calculateTargetTilt(gyroTilt, controlTilt) {
-    return gyroTilt + controlTilt
+let gyroRoll = Math.random() * 360          //replace with real input
+let controlRoll = Math.random() * 360       //replace with real input
+
+let gyroYaw = Math.random() * 360           //replace with real input
+let controlYaw = Math.random() * 360        //replace with real input
+
+
+console.log("gyroTilt: ", gyroTilt)             //for data
+console.log("controlTilt: ", controlTilt)       //for data
+
+console.log("gyroRoll: ",gyroRoll)              //for data
+console.log("controllRoll: ", controlRoll)      //for data
+
+console.log("gyroYaw: ", gyroYaw)               //for data
+console.log("controlYaw:", controlYaw)          //for data
+
+
+if(controlTilt < gyroTilt) {                    //replace with real outputs, 2d object to the right angle ccw
+    console.log("tilt down ", controlTilt - gyroTilt)
+} else if(controlTilt > gyroTilt) {             
+    console.log("tilt up ", controlTilt - gyroTilt)
+} else {
+    console.log("on targeted tilt")
 }
 
-function readGyroSensorTilt() {
-    return Math.random() * 360 - 180            
+if(controlRoll < gyroRoll) {                    //replace with real outputs, angles counted ccw
+    console.log("roll clockwise ", controlRoll - gyroRoll)
+} else if(controlRoll > gyroRoll) {
+    console.log("roll anti-clockwise ", controlRoll - gyroRoll)
+} else {
+    console.log("on targeted roll")
 }
 
-function readControlInputTilt() {
-    return Math.random() * 360 - 180            
+if(controlYaw < gyroYaw) {                      //replace with real outputs, angles counted ccw
+    console.log("yaw right ", controlYaw - gyroYaw)
+} else if(controlYaw > gyroYaw) {
+    console.log("yaw left ", controlYaw - gyroYaw)
+} else {
+    console.log("on targeted yaw")
 }
 
-function cycle() {
-    let gyroTilt = readGyroSensorTilt()
-    let controlTilt = readControlInputTilt()
-    
-    console.log("gyroTilt: ", gyroTilt);
-    console.log("controlTilt: ", controlTilt);
-    
-    const targetTilt = calculateTargetTilt(gyroTilt, controlTilt)
-    console.log("targetTilt: ", targetTilt)
-    if (targetTilt < 0) {
-        console.log("steer down")
-    } else if (targetTilt > 0) {
-        console.log("steer up")
-    } else  {
-        console.log("on target tilt")
-    }
-}
-cycle()
 
-
+gyroYaw = 0                         //has to reset real gyro outputs for implementations
+gyroTilt = 0                        //has to reset real gyro outputs for implementations
+gyroRoll = 0                        //has to reset real gyro outputs for implementations
 }
+const intervalId = setInterval(calculationCycle, 1000)
